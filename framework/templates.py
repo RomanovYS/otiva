3eaf8a067646cb9d8ccc590fefb1fe2c6148da79
Yuriy <72979855+RomanovYS@users.noreply.github.com>
2022-04-08 16:03:52 +0300

    Delete templates.py

--- framework/templates.py
+++
@@ -1,7 +0,0 @@
-from jinja2 import Environment, FileSystemLoader
-
-
-def render(path='', template='', **kwargs):
-    with open(path + template, encoding='utf-8') as f:
-        template = Environment(loader=FileSystemLoader(path)).from_string(f.read())
-    return template.render(**kwargs)
